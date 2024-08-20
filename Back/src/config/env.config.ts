c3b360f5ccaa0fbe8cb4f9c817e6db246b802ae6
Lmurillo <leobachy22@gmail.com>
2024-08-20 15:50:52 -0300

    cambios en main

--- Back/src/config/env.config.ts
+++ Back/src/config/env.config.ts
@@ -9,4 +9,4 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET; 
