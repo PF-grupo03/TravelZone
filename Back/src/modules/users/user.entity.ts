b9030e2c9dad40b1172ff5003604a877d8a03c77
camilo Navas <navasochoa@hotmail.com>
2024-08-21 11:28:28 -0500

    combinando merge, uuid id string

--- Back/src/modules/users/user.entity.ts
+++ Back/src/modules/users/user.entity.ts
@@ -1,7 +1,5 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { OrderEntity } from '../orders/order.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { OrderEntity } from '../orders/order.entity';
 
 @Entity('users')
 export class UserEntity {
@@ -10,64 +8,38 @@ export class UserEntity {
 
   @Column()
   username: string;
-  @Column()
-  username: string;
 
-  @Column()
-  name: string;
   @Column()
   name: string;
 
   @Column()
   email: string;
-  @Column()
-  email: string;
 
-  @Column()
-  password: string;
   @Column()
   password: string;
 
   @Column()
   dni: number;
-  @Column()
-  dni: number;
 
-  @Column()
-  phone: number;
   @Column()
   phone: number;
 
   @Column()
   address: string;
-  @Column()
-  address: string;
 
-  @Column()
-  country: string;
   @Column()
   country: string;
 
   @Column()
   city: string;
-  @Column()
-  city: string;
 
-  @Column()
-  birthday: Date;
   @Column()
   birthday: Date;
 
   @Column()
   IsAdmin: boolean;
   default = false;
-  @Column()
-  IsAdmin: boolean;
-  default = false;
 
-  @OneToMany(() => OrderEntity, (order) => order.user)
-  order?: OrderEntity[];
   @OneToMany(() => OrderEntity, (order) => order.user)
   order?: OrderEntity[];
 }
-
