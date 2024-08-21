efdc73115c91d7ae54fa353dddc3906f7a6632ac
Thiago Milanca <thiagomilanca@gmail.com>
2024-08-21 10:53:56 -0300

    Entidades

---
+++ Back/src/modules/products/product.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { CategoryEntity } from "../categories/category.entity";
+import { OrderDetailsEntity } from "../orders/orderDetails.entity";
+
+@Entity('products')
+export class ProductEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    stock: number;
+
+    @Column()
+    imgUrl: string;
+
+    @Column()
+    categoryId: string;
+
+    @ManyToOne(() => CategoryEntity, (category) => category.products)
+    category: CategoryEntity;
+
+    @ManyToMany(() => OrderDetailsEntity, (orderDetails) => orderDetails.product)
+    orderDetails: OrderDetailsEntity[];
+}
