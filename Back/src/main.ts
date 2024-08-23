a4324d9dbd8430029d2adf9fc3e244e34fed95c4
Lmurillo <leobachy22@gmail.com>
2024-08-22 21:24:24 -0300

    conexion BD

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const PORT= process.env.DB_PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
   }));
-  await app.listen(3006);
+  await app.listen(PORT);
 }
 bootstrap();
