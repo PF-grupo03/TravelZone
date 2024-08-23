ed788bf143b22f2d11db9116cfcadb27e6148ae6
Camilo Navas <navasochoa@hotmail.com>
2024-08-23 13:03:20 -0500

    imagenes en cloudinary se guardan en carpeta

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -2,12 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 9d4686bb99a9b849a7dbb41db303ea245f72514c
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,19 +11,16 @@ async function bootstrap() {
     .setDescription('Proyecto Final Grupo03 Travel Zone')
     .setVersion('1.0.0')
     .addBearerAuth()
-    .build()
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-  }));
-<<<<<<< HEAD
-  await app.listen(3006);
-=======
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
   console.log('connection to the server succesful');
-  
->>>>>>> 9d4686bb99a9b849a7dbb41db303ea245f72514c
 }
 bootstrap();
--- Back/src/modules/products/products.module.ts
+++ Back/src/modules/products/products.module.ts
@@ -4,9 +4,13 @@ import { ProductsService } from './products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './product.entity';
 import { ProductsRepository } from './product.repository';
+import { CategoryEntity } from '../categories/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity]),
+    TypeOrmModule.forFeature([CategoryEntity]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
 })
