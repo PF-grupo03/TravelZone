4458a1c680564d8f737e49580136a76494315ba7
Lmurillo <leobachy22@gmail.com>
2024-08-23 15:44:58 -0300

    ruta orders, termino de auth google

--- Back/src/modules/orders/orders.controller.ts
+++ Back/src/modules/orders/orders.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateOrderDto } from './orders.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor (private readonly orderService: OrdersService) {}
+
+    @ApiOperation({ summary: 'Agregar una nueva orden', description: 'Crea una nueva orden para el usuario actual.' })
+    @ApiBearerAuth()
+    @Post()
+    @UseGuards(AuthGuard)
+    addOrder(@Body() order: CreateOrderDto) {
+        const { userId, products} = order;
+        return this.orderService.addOrder(userId, products);
+    }
+
+    @ApiOperation({ summary: 'Obtener una orden por ID', description: 'Obtiene los detalles de una orden específica por su ID.' })
+    @ApiBearerAuth()
+    @Get(':id')
+    @UseGuards(AuthGuard)
+    getOrder(@Param('id', ParseUUIDPipe) id: string) {
+        return this.orderService.getOrder(id);
+    }
+}
--- Back/src/modules/orders/orders.service.ts
+++ Back/src/modules/orders/orders.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(private  ordersRepository: OrdersRepository){}
+    
+    addOrder(userId: string, product: any) {
+        return this.ordersRepository.addOrder(userId, product);
+    }
+
+    getOrder(id: string) {
+        return this.ordersRepository.getOrder(id);
+    }
+}
