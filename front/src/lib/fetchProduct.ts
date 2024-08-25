284de79180a89e8119b20eb5e1fdeb6cb12c2d34
Agustin Ferigni <gustinigni@gmail.com>
2024-08-25 20:37:40 -0300

    Login y Register

--- front/src/lib/fetchProduct.ts
+++ front/src/lib/fetchProduct.ts
@@ -1,15 +1,15 @@
 import { IProduct } from "@/types";
 
 export async function fetchProducts(): Promise<IProduct[]> {
-    const response = await fetch("http://localhost:3001/products")
-    const products = await response.json();
-    return products;
-    
+  const response = await fetch("https://pf-grupo03-back.onrender.com/products");
+  const products = await response.json();
+  return products;
 }
 
-export async function fetchProductById(id: string): Promise <IProduct>{
-    const response = await fetch(`http://localhost:3001/products/${id}`)
-    const product = await response.json();
-    return product;
-    
-  }
+export async function fetchProductById(id: string): Promise<IProduct> {
+  const response = await fetch(
+    `https://pf-grupo03-back.onrender.com/products/${id}`
+  );
+  const product = await response.json();
+  return product;
+}
