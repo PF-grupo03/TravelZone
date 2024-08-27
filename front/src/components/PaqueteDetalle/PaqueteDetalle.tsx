5b4f12d43a7b0108c1da0447a2c64f89767b2833
yaakov <diegofe.rodriguez@usa.edu.co>
2024-08-27 15:00:35 -0500

    Arreglo en las imagenes del detalle de paquete

--- front/src/components/PaqueteDetalle/PaqueteDetalle.tsx
+++ front/src/components/PaqueteDetalle/PaqueteDetalle.tsx
@@ -3,37 +3,49 @@
 import TarjetaPage from "../armatupaquete";
 
 const PaqueteDetalle = ({
-  id,
-  image,
-  image2,
-  image3,
-  title,
-  description,
-  description2,
-  price,
-  location,
-  duration,
-  stock,
-  categories,
+	id,
+	image,
+	image2,
+	image3,
+	title,
+	description,
+	description2,
+	price,
+	location,
+	duration,
+	stock,
+	categories,
 }) => {
-  return (
-    <div className="">
-      <div className="my-16 flex flex-col items-center justify-center w-full">
-        <div className="flex justify-center space-x-6 h-full mt-6">
-          <img src={image} alt={`${title} image 1`} className="h-96 w-full" />
-          <img src={image2} alt={`${title} image 2`} className="h-96 w-full" />
-          <img src={image3} alt={`${title} image 3`} className="h-96 w-full" />
-        </div>
-        <div className="flex my-20 items-center text-center justify-evenly">
-          <div>
-            <h1 className="text-4xl font-bold mb-4">{title}</h1>
-            <p className="text-black text-opacity-50">{description}</p>
-          </div>
-        </div>
-      </div>
-      <TarjetaPage title={title} description2={description2} price={price} />
-    </div>
-  );
+	return (
+		<div className="-mx-[110px] flex flex-col">
+			<div className="my-16 flex flex-col items-center justify-center w-full bg-slate-200 min-w-20">
+				<div className="flex justify-center space-x-6 h-full mt-6 max-w-full overflow-hidden mx-10">
+					<img
+						src={image}
+						alt={`${title} image 1`}
+						className="h-96 w-full  object-contain"
+					/>
+					<img
+						src={image2}
+						alt={`${title} image 2`}
+						className="h-96 w-full  object-contain"
+					/>
+					<img
+						src={image3}
+						alt={`${title} image 3`}
+						className="h-96 w-full  object-contain"
+					/>
+				</div>
+				<div className="flex my-20 items-center text-center justify-evenly">
+					<div>
+						<h1 className="text-4xl font-bold mb-4">{title}</h1>
+						<p className="text-black text-opacity-50">{description}</p>
+					</div>
+				</div>
+			</div>
+			<TarjetaPage title={title} description2={description2} price={price} />
+		</div>
+	);
 };
 
 export default PaqueteDetalle;
