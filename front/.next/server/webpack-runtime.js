9c9ab515bdf35039cf554ed4f2a681dba0c959f8
yaakov <diegofe.rodriguez@usa.edu.co>
2024-08-27 21:31:50 -0500

    Archivos faltantes en el gitignore

--- front/.next/server/webpack-runtime.js
+++ front/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2ef739cc6ce5eeab")
+/******/ 		__webpack_require__.h = () => ("32cced47dcdcf8c9")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
