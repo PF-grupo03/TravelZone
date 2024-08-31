50cd0d9b9b1b2c22f85999bf746fb1bb7d8a06fc
yaakov <diegofe.rodriguez@usa.edu.co>
2024-08-31 07:29:47 -0500

    Se creó el componente y la página de checkout

---
+++ front/src/app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import Checkout from "@/components/Checkout/Checkout";
+import React from "react";
+
+const page = () => {
+	return (
+		<div>
+			<Checkout />
+		</div>
+	);
+};
+
+export default page;
